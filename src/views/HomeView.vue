df88acb29ddb08f097e1a9277324f20d15ffe7c5
Suphakit P. <me@suphakit.net>
2025-12-07 22:44:18 +0700

    Squashed 'frontend/' changes from 12b754e..a7a675d
    a7a675d fixed<pages>: onload error
    
    git-subtree-dir: frontend
    git-subtree-split: a7a675d2a6553dde51c30060b03cfc790e1c94f7

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,6 +13,9 @@ const fullBankConfig = ref<BankingInterface | BankingInterface[] | null>(null)
 const bankConfig = ref<BankingInterface | null>(null)
 const state = ref<number>(0)
 
+const isLoading = ref<boolean>(true)
+const error = ref<String>('')
+
 const bankingUpdate = () => {
   let itemPayload: BankingInterface | null | undefined = null
 
@@ -39,31 +42,48 @@ onMounted(async () => {
   const checkConfig = await isConfig(true)
 
   if (checkConfig) {
-    const configFetch = await (
-      await fetch(`${import.meta.env.VITE_API_ENDPOINT}/api/config`)
-    ).json()
+    try {
+      const configFetch = await (
+        await fetch(`${import.meta.env.VITE_API_ENDPOINT}/api/config`)
+      ).json()
 
-    const payload = configFetch.payload as OverviewInterface
+      const payload = configFetch.payload as OverviewInterface
 
-    config.value = payload
+      config.value = payload
 
-    if (config.value === null) {
-      return
+      if (config.value === null) {
+        return
+      }
+
+      if (config.value.banking === null) {
+        return
+      }
+
+      fullBankConfig.value = config.value.banking
+
+      bankingUpdate()
+    } catch (e) {
+      const err = e as Error
+
+      error.value = err.message
+    } finally {
+      isLoading.value = false
     }
-
-    if (config.value.banking === null) {
-      return
-    }
-
-    fullBankConfig.value = config.value.banking
-
-    bankingUpdate()
   }
 })
 </script>
 
 <template>
-  <main>
+  <main v-if="isLoading && error === ''" class="loading-container">
+    <h1>Loading..</h1>
+  </main>
+
+  <main v-if="error !== ''">
+    <h1>Error</h1>
+    <p>{{ error }}</p>
+  </main>
+
+  <main v-if="!isLoading && error === ''" class="loading-container">
     <DonateSetup
       :bank="bankConfig"
       :user="config !== undefined && config !== null ? config.detailed : null"
@@ -75,3 +95,14 @@ onMounted(async () => {
     />
   </main>
 </template>
+
+<style scoped>
+.loading-container {
+  width: 100%;
+  height: 100dvh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
